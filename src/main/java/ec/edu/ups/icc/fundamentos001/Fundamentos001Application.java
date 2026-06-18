6d1a270263eaf4ce49803f5556ab666334ea7eeb
BryanM112 <estbryanmc@gmail.com>
2026-06-18 18:49:51 -0500

    implementar endpoint api/status con Spring Boot Kotlin DSL

---
+++ src/main/java/ec/edu/ups/icc/fundamentos001/Fundamentos001Application.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.icc.fundamentos001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos001Application.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/ups/icc/fundamentos001/Fundamentos001ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.icc.fundamentos001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
